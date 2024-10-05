4d711c12cc95fab5c090b20479a48e1491b7975a
LordCogsWorth <kyle.price.ra@gmail.com>
2024-10-05 05:24:58 -0400

    Made changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import LoggedIn from "./components/LoggedIn";
+import LoggedOut from "./components/LoggedOut";
+
+export default function App() {
+  const { isLoading, isAuthenticated } = useKindeAuth();
+
+  if (isLoading) return <>Loading...</>;
+
+  return isAuthenticated ? <LoggedIn /> : <LoggedOut />;
+
+  
+  
+  if (isAuthenticated) return <><Dashboard></Dashboard></>;
+
+}
