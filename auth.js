a697938a3fe14c886ee9c8967a8aac518f9830a8
LordCogsWorth <kyle.price.ra@gmail.com>
2024-10-06 09:00:47 -0400

    testing kinde

--- auth.js
+++ auth.js
@@ -2,4 +2,3 @@ VITE_KINDE_CLIENT_ID=dd216122ded249029786063571c137f4
 VITE_KINDE_DOMAIN=https://sensai.kinde.com
 VITE_KINDE_REDIRECT_URL=https://poetic-cassata-6b6805.netlify.app
 VITE_KINDE_LOGOUT_URL=https://poetic-cassata-6b6805.netlify.app
-//
