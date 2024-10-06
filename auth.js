b99cfde4981ef6e1726f6b48b1d469a412256f8a
LordCogsWorth <kyle.price.ra@gmail.com>
2024-10-06 08:45:48 -0400

    fix

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
 VITE_KINDE_CLIENT_ID=dd216122ded249029786063571c137f4
 VITE_KINDE_DOMAIN=https://sensai.kinde.com
-VITE_KINDE_REDIRECT_URL=http://localhost:3000
-VITE_KINDE_LOGOUT_URL=http://localhost:3000
+VITE_KINDE_REDIRECT_URL=https://sensaikh24.netlify.app/
+VITE_KINDE_LOGOUT_URL=https://sensaikh24.netlify.app/
