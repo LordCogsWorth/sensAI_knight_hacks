6934230fdb619b01e3f9b8a470ea65dfa98112a0
LordCogsWorth <kyle.price.ra@gmail.com>
2024-10-06 08:53:46 -0400

    Fix?

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
 VITE_KINDE_CLIENT_ID=dd216122ded249029786063571c137f4
 VITE_KINDE_DOMAIN=https://sensai.kinde.com
-VITE_KINDE_REDIRECT_URL=https://sensaikh24.netlify.app/
-VITE_KINDE_LOGOUT_URL=https://sensaikh24.netlify.app/
+VITE_KINDE_REDIRECT_URL=https://poetic-cassata-6b6805.netlify.app
+VITE_KINDE_LOGOUT_URL=https://poetic-cassata-6b6805.netlify.app
+//
